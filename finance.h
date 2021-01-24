a87a93a39eb4bc4cc9b041e2683ff168b3fd8ea3
YI75 <yibarra000@citymail.cuny.edu>
2021-01-23 22:58:49 -0500

    Saved

--- finance.h
+++ finance.h
@@ -6,22 +6,28 @@ namespace ibarra_tan_james{
     class personalFinance{
         public:
         personalFinance();
-        double net_worth const;
-        void budget const;
+        double round(double value);
+        double net_worth() const;
+        void budget() const;
+        void current()  const;
+        friend personalFinance operator+(const personalFinance& s1, const personalFinance& s2);
 
 
         private:
+        double salary;
         //Assets
+        double sum_assets;
         double real_estate;
         double checking_account;
         double savings_account;
         double retirement_account;
         //Liabilites
+        double sum_liabilities;
         double mortgages;
         double credit_card_debt;
         double personal_loans;
         double student_loans;
         double auto_loans;
-        double yerrrr;
     }
 }
+#endif
